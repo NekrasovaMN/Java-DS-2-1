41e6c9f698c5cc9cdffb525f59c2f8eea60119dd
Margarita Nekrasova <nekr43@mail.ru>
2023-07-22 12:00:31 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+
+    public static void main(String[] args) {
+        int amount = 13676;
+        int rub = 20;
+
+        System.out.println("Количество начисленных миль за купленный билет: " + amount / rub * 1);
+    }
+}
